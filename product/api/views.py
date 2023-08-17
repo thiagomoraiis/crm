eb97a3a6838da06caa07a2fa011a75222a0bb7ed
thiagomoraiis <thiagomorais2605@gmail.com>
2023-08-17 20:13:45 -0300

    feat: criando uma 'api_view' para cada metodo de CRUD

--- product/api/views.py
+++ product/api/views.py
@@ -6,54 +6,66 @@ from .serializers import ProductModelSerializer
 from rest_framework import status # noqa
 
 
-@api_view(http_method_names=['GET', 'POST'])
+@api_view(['GET'])
 def product_api_list(request):
-    if request.method == 'GET':
-        products = Product.objects.all()[0:11].\
-            prefetch_related('tags')
-        serializer = ProductModelSerializer(
-            instance=products, many=True,
-            context={'request': request},
-        )
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = ProductModelSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
-        else:
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
+    products = Product.objects.all()[0:11].\
+        prefetch_related('tags')
+    serializer = ProductModelSerializer(
+        instance=products, many=True,
+        context={'request': request},
+    )
+    return Response(serializer.data)
 
 
-@api_view(http_method_names=['GET', 'PUT', 'DELETE'])
+@api_view(['POST'])
+def product_api_create(request):
+    serializer = ProductModelSerializer(
+        data=request.data, context={'request': request}
+    )
+
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
 def product_api_detail(request, id):
-    if request.method == 'GET':
-        product = get_object_or_404(
-            Product.objects.filter(id=id),
-            id=id
-        )
-        serializer = ProductModelSerializer(
-            instance=product, many=False
-        )
+    product = get_object_or_404(
+        Product.objects.filter(id=id),
+        id=id
+    )
+    serializer = ProductModelSerializer(
+        instance=product, many=False
+    )
+    return Response(
+        serializer.data
+    )
+
+
+@api_view(['PUT'])
+def product_api_update(request, id):
+    product = get_object_or_404(
+        Product.objects.filter(id=id), id=id
+    )
+    serializer = ProductModelSerializer(
+        instance=product, data=request.data
+    )
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
         return Response(
-            serializer.data
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
-    elif request.method == 'PUT':
-        pass
-    elif request.method == 'DELETE':
-        product = get_object_or_404(
-            Product.objects.filter(id=id),
-            id=id
-        )
-        product.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT,
-        )
+
+
+@api_view(['DELETE'])
+def product_api_delete(request, id):
+    product = get_object_or_404(
+        Product.objects.filter(id=id),
+        id=id
+    )
+    product.delete()
+    return Response(
+        status=status.HTTP_204_NO_CONTENT,
+    )
