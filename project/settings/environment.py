3c0454ff813ebab50e60b5a3f3dd615418707af7
thiagomoraiis <thiagomorais2605@gmail.com>
2023-08-16 22:14:11 -0300

    refactor: organizando arquivo settings em um package

---
+++ project/settings/environment.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import os # noqa
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-kv6=vxp5_lkiqqp*1fl$*ooljo(=nmpiyo)-o2_+*s(%*+qnob' # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+ROOT_URLCONF = 'project.urls'
+
+WSGI_APPLICATION = 'project.wsgi.application'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'custom_user.CustomUser'
---
+++ project/settings/installed_apps.py
@@ -0,0 +1,23 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'bootstrap5',
+    'corsheaders',
+
+    'custom_user',
+    'core',
+    'customer',
+    'cart',
+    'product',
+    # 'opportunities',
+    # 'interactions',
+    # 'administrator',
+    # 'tasks',
+]
