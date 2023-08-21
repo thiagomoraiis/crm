d57ab65f936ff605ed67631d56168d1b2c3de710
thiagomoraiis <thiagomorais2605@gmail.com>
2023-08-21 14:01:07 -0300

    criando ModelSerializer de 'User'

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
 from ..models import Inventory, Invoicing
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id', 'first_name', 'last_name',
+            'username', 'email', 'password'
+        ]
 
 
 class InventoryModelSerializer(serializers.ModelSerializer):
